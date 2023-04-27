e605366746ec0d3fdcaa8f474e33977ef64d49c7
krasenHristov <doanusho@gmail.com>
2023-04-27 16:54:59 +0100

    js vars

---
+++ variables.js
@@ -0,0 +1,38 @@
+let variable = 'test';
+
+console.log(variable);
+
+variable = 'another value';
+
+console.log(variable);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+	
+	sum += i;
+}
+
+console.log(sum);
+
+const constant = 'test';
+
+console.log(constant);
+
+
+let numberLiteral = 30;
+let boolean = true;
+let string = 'test';
+let undefinedVariable = undefined;
+let nullVariable = null;
+
+// object is a collection of key-value pairs
+// similar to a dictionary in Python
+let object = {
+	literal: 30,
+	boolean: true,
+	string: 'test',
+	undefinedVariable: undefined,
+	nullVariable: null
+};
+
+console.log(object);
