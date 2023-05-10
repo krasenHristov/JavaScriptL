eb4dc939470e85613a6f606b03320c62667440b5
krasenHristov <doanusho@gmail.com>
2023-05-10 17:09:21 +0100

    more reduce

--- ES6/reduce.js
+++ ES6/reduce.js
@@ -45,3 +45,57 @@ let beatles2 = names.reduce((accumulator, beatle) => {
   accumulator.push(beatle.name);
     return accumulator;
 }, []);
+
+
+var desks = [
+  { type: 'sitting' },
+  { type: 'standing' },
+  { type: 'sitting' },
+  { type: 'sitting' },
+  { type: 'standing' }
+];
+
+const deskTypes = desks.reduce(function(obj, desk) {
+  if (desk.type === 'sitting') {
+    obj.sitting++;
+  }
+  if (desk.type === 'standing') {
+    obj.standing++;
+  }
+  return obj;
+  }, { sitting:0, standing:0});
+
+console.log(deskTypes); // { sitting: 3, standing: 2 }
+
+
+const numbers2 = [1, 1, 2, 3, 4, 4];
+
+// [] is the initial value of the accumulator (an empty array)
+// if the number is not in the array, push it to the array
+// if the number is in the array, don't push it to the array
+// the -1 is the index of the number in the array if it is in the array 
+// if the number is not in the array, the index is will be -1
+function unique(array) {
+  return array.reduce(function (newArray, number) {
+    if (newArray.indexOf(number) === -1) {
+      newArray.push(number);
+    }
+    return newArray;
+  }, []);
+}
+
+console.log(unique(numbers2)); // [1, 2, 3, 4]
+
+// with reduce and find
+function unique2(array) {
+  return array.reduce(function (newArray, number) {
+    if (!newArray.find(function (newNumber) {
+      return newNumber === number;
+    })) {
+      newArray.push(number);
+    }
+    return newArray;
+  }, []);
+}
+
+console.log(unique2(numbers2)); // [1, 2, 3, 4]
--- ES6/reduceExercises.js
+++ ES6/reduceExercises.js
@@ -11,27 +11,53 @@ const notBalanced = "[[[[]]]]]]]" // not balanced
 // Write a function that takes a string and returns true if the string is balanced
 // and false if it is not balanced
 
-function isBalanced(str) {
+function isBalancedFor(str) {
   let open = ["(", "[", "{"];
   let close = [")", "]", "}"];
   let count = 0;
 
   for (let i = 0; i < str.length; i++) {
     if (open.includes(str[i])) {
-      console.log(str[i]);
       count++;
     }else if(close.includes(str[i])) {
-      console.log(str[i]);
       count--;
     }};
   if (count < 0 || count > 0) {
-    console.log(count);
     return false;
 } else {
-    console.log(count);
     return true;
 }};
 
-console.log(isBalanced(balanced));
-console.log(isBalanced(notBalanced));
+console.log("for loop balanced", isBalancedFor(balanced));
+console.log("for loop not ballanced", isBalancedFor(notBalanced));
 
+// ES6 with reduce
+function isBalancedRed(str) {
+  return !str.split("").reduce((PrevChar, thisChar) => {
+    if (thisChar === "(" || thisChar === "[" || thisChar === "{") {
+      return ++PrevChar;
+    };
+    if (thisChar === ")" || thisChar === "]" || thisChar === "}") {
+      return --PrevChar;
+    };
+    return PrevChar;
+  }, 0)};
+
+console.log("reduce balanced", isBalancedRed(balanced));
+console.log("reduce not balanced", isBalancedRed(notBalanced));
+
+// with arrow function
+// 0 is the initial value of PrevChar(accumulator)
+const isBalancedArr = (str) => {
+  return !str.split("").reduce((PrevChar, thisChar) => {
+    if (thisChar === "(" || thisChar === "[" || thisChar === "{") {
+      return ++PrevChar;
+    };
+    if (thisChar === ")" || thisChar === "]" || thisChar === "}") {
+      return --PrevChar;
+    };
+    return PrevChar;
+  }, 0)}
+
+console.log("red arrow balanced", isBalancedArr(balanced));
+console.log("red arrow not balanced", isBalancedArr(notBalanced));
